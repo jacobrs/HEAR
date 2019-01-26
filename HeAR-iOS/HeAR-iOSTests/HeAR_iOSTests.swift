8088d7a1c45feae975d3c58ee2f4b4a6cc53cf4b
Francesco Valela <valela.francesco@gmail.com>
2019-01-26 11:53:40 -0500

    initial commit | iOS

---
+++ HeAR-iOS/HeAR-iOSTests/HeAR_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  HeAR_iOSTests.swift
+//  HeAR-iOSTests
+//
+//  Created by Francesco Valela on 2019-01-26.
+//  Copyright © 2019 Francesco Valela. All rights reserved.
+//
+
+import XCTest
+@testable import HeAR_iOS
+
+class HeAR_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# HeAR
-Augmented reality application that allows people hard of hearing to see conversational subtitles through their phone's camera.
